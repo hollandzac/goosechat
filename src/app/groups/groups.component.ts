7e6b8e45e9c7543e655b8660fdfed2ccb47e5240
Zachary Holland <holland.zac@gmail.com>
2021-09-08 15:50:54 +1000

    implmented main group page

--- src/app/groups/groups.component.ts
+++ src/app/groups/groups.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService, Group, Role, Channel } from '../services/storage.service';
 
 @Component({
   selector: 'app-groups',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupsComponent implements OnInit {
 
-  constructor() { }
+  newGroupName: string = ""
+  newChannelName: string = ""
+  newGroupError: boolean = false
+  newChannelErr: boolean = false
+
+  constructor(public storageService: StorageService) { }
 
   ngOnInit(): void {
+    this.storageService.getData()
+  }
+  addNewGroup(){
+    this.newGroupError = this.storageService.newGroup(this.newGroupName)
+    this.newGroupName = ""
+  }
+  addNewChannel(group: Group){
+    this.newChannelErr = this.storageService.newChannel(this.newChannelName, group)
+    this.newChannelName = ""
+  }
+  removeChannel(group: Group, channel: Channel){
+    this.storageService.removeChannel(channel, group)
   }
 
 }
--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,91 +1,158 @@
 import { Injectable } from '@angular/core';
 
-
 export enum Role {
-  User = "user",
-  SuperAdmin = "superadmin",
-  GroupAdmin = "groupadmin",
-  GroupAssistant = "groupassis"
+  User = 'user',
+  SuperAdmin = 'superadmin',
+  GroupAdmin = 'groupadmin',
+  GroupAssistant = 'groupassis',
 }
 export interface Channel {
-  id:number
-  name: string
-  users: Array<number>
+  name: string;
+  users: Array<number>;
 }
 export interface Group extends Channel {
-  id: number
-  channels: Array<Channel>
+  channels: Array<Channel>;
 }
 
 export interface User {
-  username: string
-  email: string
-  id: number
-  role: Role
+  username: string;
+  email: string;
+  id: number;
+  role: Role;
 }
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class StorageService {
-  public groups: Array<Group> = []
-  public users: Array<User> = []
+  public groups: Array<Group> = [];
+  public users: Array<User> = [];
 
   constructor() {
-    this.getData()   
   }
-  getData(){
-    let users = localStorage.getItem('users')
-    let groups = localStorage.getItem('groups')
-    if(users !== null){
-      this.users = JSON.parse(users)
+  createSuper(){
+    let superUser: User = {
+      username: "super",
+      email: "super@goosechat.com",
+      id: 1,
+      role: Role.SuperAdmin,
+
     }
-    if(groups !== null){
-      this.groups = JSON.parse(groups)
+    this.users.push()
+  }
+  getData() {
+    let users = localStorage.getItem('users');
+    let groups = localStorage.getItem('groups');
+    if (users !== null) {
+      this.users = JSON.parse(users);
+    }
+    if (groups !== null) {
+      this.groups = JSON.parse(groups);
     }
   }
 
-  storeData(){
-    localStorage.setItem("groups", JSON.stringify(this.groups))
-    localStorage.setItem("users", JSON.stringify(this.users))
+  storeData() {
+    localStorage.setItem('groups', JSON.stringify(this.groups));
+    localStorage.setItem('users', JSON.stringify(this.users));
   }
 
-  getUser(username: string): User | string{
-    let user = this.users.find(user => user.username === username)
-    if(user) {
-      return user
+  getUser(username: string): User | string {
+    let user = this.users.find((user) => user.username === username);
+    if (user) {
+      return user;
     }
-    return ""
+    return '';
   }
 
-  newUser(user: User){
-    this.users.push(user)
+  newUser(user: User) {
+    this.users.push(user);
   }
 
-  newChannel(channelID: number, groupID: number){
+  newChannel(channelName: string, group: Group) {
+    let idxGr = group.channels.findIndex(channel => channel.name === channelName)
+    if( idxGr === -1){
+      let newChannel: Channel = {
+        name: channelName,
+        users: [],
+      }
+      group.channels.push(newChannel);
+      return false
+    }
+    return true
   }
-  removeChannel(channelID: number, groupID: number){
+  removeChannel(channel: Channel, group: Group) {
+  
+    let idxCh = group.channels.indexOf(channel);
+    group.channels.splice(idxCh,1)
   }
 
-  newGroup(group: Group){  
+  newGroup(groupName: string):boolean {
+    let idxGr = this.groups.findIndex(group => group.name === groupName)
+    if( idxGr === -1){
+      let newGroup: Group = {
+        name: groupName,
+        users: [],
+        channels: [],
+      }
+      this.groups.push(newGroup);
+      return false
+    }
+    return true
   }
-  removeGroup(groupid: number){}
-  addUserToGroup(user:User, groupID: number){
-  }
-  removeUserFromGroup(userID: number, groupID: number){
-  }
-  addUserToChannel(user:User, channelID: number, groupID: number){
-  }
-  removeUserFromChannel(userID: number, channelID: number, groupID: number){
-  }
-  setUserRole(userID: number, role: Role){
+
+  // removeGroup(groupid: number) {
+  //   this.groups.splice(this.findGroupWithID(groupid));
+  // }
+
+  // addUserToGroup(userID: number, groupID: number) {
+  //   this.groups[this.findGroupWithID(groupID)].users.push(userID);
+  // }
+
+  // removeUserFromGroup(userID: number, groupID: number): boolean {
+  //   let idxGr = this.findGroupWithID(groupID);
+
+  //   if (idxGr === -1) {
+  //     return false;
+  //   }
+  //   let idxUs = this.groups[idxGr].users.indexOf(userID);
+  //   this.groups[idxGr].users.splice(idxUs);
+
+  //   this.groups[idxGr].channels.forEach((channel) => {
+  //     if (channel !== null) {
+  //       let idx = channel.users.indexOf(userID);
+  //       channel.users.splice(idx);
+  //     }
+  //   });
+
+  //   return true;
+  // }
+
+  // addUserToChannel(userId: number, channelID: number, groupID: number){
+  //   let group = this.groups[this.findGroupWithID(groupID)]
     
-  }
+  //   if(group.users.indexOf(userId) === -1){
+  //     this.addUserToGroup(userId, groupID)
+  //   }
+  //   group.channels[this.findChannelWithID(channelID, group)].users.push(userId)
+  // }
+  
+  // removeUserFromChannel(userID: number, channelID: number, groupID: number) {
+  //   let group = this.groups[this.findGroupWithID(groupID)]
+  //   let channel =  group.channels[this.findChannelWithID(channelID, group)]
 
+  //   let idxUs =  channel.users.indexOf(userID)
+    
+  //   channel.users.splice(idxUs, 1)
 
+  // }
+  // setUserRole(userID: number, role: Role){
+  //   this.users[this.users.findIndex(user => user.id === userID)].role = role
+  // }
 
-
-
+  // findGroupWithID(groupID: number): number {
+  //   return this.groups.findIndex((group) => group.id === groupID);
+  // }
+  // findChannelWithID(channelID: number, group: Group): number{
+  //   return group.channels.findIndex(channel => channel.id === channelID)
+  // }
 }
